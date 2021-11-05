dac18026b3512153baea8f0bf4bc34371bfccb9f
Cedric Verstege <cedric.verstege@student.kit.edu>
2021-11-05 09:57:32 +0100

    zsh compatibility for ini_KappaTools.sh

--- Toolbox/scripts/ini_KappaTools.sh
+++ Toolbox/scripts/ini_KappaTools.sh
@@ -1,30 +1,23 @@
 #!/bin/bash
 
-if [ -n "$BASH_VERSION" ]; then
+if [ ! -z "$ZSH_VERSION" ]; then
+	this_file="${(%):-%x}"
+else
+	this_file="${BASH_SOURCE[0]}"
+fi
 
-	# get path of KappaTools installation for bash
-	if [[ ${BASH_SOURCE[0]} == *"${CMSSW_BASE}/bin/"* || ${BASH_SOURCE[0]} == *"${CMSSW_BASE}/src/KappaTools/"* ]]; then
-		export KAPPATOOLSPATH=$CMSSW_BASE/src/KappaTools
-	else
-		# get KappaTools dir relative to location of this script for bash
-		export KAPPATOOLSPATH=$(readlink -f $(dirname $(readlink -f ${BASH_SOURCE[0]}))/../..)
-	fi
 
-elif [ -n "$ZSH_VERSION" ]; then
-
-	# get path of KappaTools installation for zsh
-	if [[ ${0} == *"${CMSSW_BASE}/bin/"* || ${0} == *"${CMSSW_BASE}/src/KappaTools/"* ]]; then
-		export KAPPATOOLSPATH=$CMSSW_BASE/src/KappaTools
-	else
-		# get KappaTools dir relative to location of this script for zsh
-		export KAPPATOOLSPATH=$(readlink -f -- $(dirname  -- "$(readlink -f -- "${0}")")/../..)
-	fi
-
+# get path of KappaTools installation for bash
+if [[ "$this_file" == *"${CMSSW_BASE}/bin/"* || "$this_file" == *"${CMSSW_BASE}/src/KappaTools/"* ]]; then
+        export KAPPATOOLSPATH="$CMSSW_BASE/src/KappaTools"
+else
+        # get KappaTools dir relative to location of this script for bash
+        export KAPPATOOLSPATH=$(readlink -f $(dirname $(readlink -f "$this_file"))/../..)
 fi
 
 export KAPPAPATH=$(readlink -f $KAPPATOOLSPATH/../Kappa)
 
-if [[ ! -n ${CMSSW_BASE} ]] || [[ ! $(dirname $(readlink -mf ${BASH_SOURCE[0]})) == ${CMSSW_BASE}* ]];  # i.e. 'if installed inside CMSSW'
+if [[ ! -n ${CMSSW_BASE} ]] || [[ ! $(dirname $(readlink -mf "$this_file")) == ${CMSSW_BASE}* ]];  # i.e. 'if installed inside CMSSW'
 then
 	# configurations needed for compilation of C++ code
 	#export BOOSTPATH=$(ls /afs/cern.ch/cms/${SCRAM_ARCH}/external/boost/* -d | tail -n 1)/
